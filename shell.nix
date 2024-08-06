13c449a990758365f93059a1619443f33da575b1
Ileska <ileska@tutanota.com>
2024-08-06 16:03:25 +0300

    Add shell nix

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {}}:
+  pkgs.mkShell {
+    nativeBuildInputs = let
+      env = pyPkgs : with pyPkgs; [
+      ];
+    in with pkgs; [
+      (python311.withPackages env)
+      arduino
+  ];
+}
